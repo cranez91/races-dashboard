20756beeb5524e69c7df259a78108ade2e1af71d
Néstor Ruiz <n.raulrg@gmail.com>
2025-08-25 18:38:26 -0600

    chore: add database seeders for initial test data

---
+++ database/factories/RaceFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Race>
+ */
+class RaceFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->randomElement(['5K', '10K']),
+            'date' => $this->faker->dateTimeBetween('now', '+3 months'),
+        ];
+    }
+}
---
+++ database/factories/RegistrationFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Participant;
+use App\Models\Race;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Registration>
+ */
+class RegistrationFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'participant_id' => Participant::factory(),
+            'race_id' => Race::factory(),
+            'category' => $this->faker->randomElement(['general', 'master']),
+        ];
+    }
+}
